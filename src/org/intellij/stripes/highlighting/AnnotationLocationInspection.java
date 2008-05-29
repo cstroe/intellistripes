3405a63b51ad48a14d46cc5c3ceb2fc55a86488e
Mario.Arias.C <Mario.Arias.C@57c81fc5-d53a-0410-bfd8-01a933467fb7>
2008-05-29 04:37:58 +0000

    Apache License and a bugfix on SpringBean Annotation Inspection

--- src/org/intellij/stripes/highlighting/AnnotationLocationInspection.java
+++ src/org/intellij/stripes/highlighting/AnnotationLocationInspection.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2000-2007 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.intellij.stripes.highlighting;
 
 import com.intellij.codeHighlighting.HighlightDisplayLevel;
@@ -110,7 +127,7 @@ public class AnnotationLocationInspection extends LocalInspectionTool {
                             : LocalQuickFix.EMPTY_ARRAY);
                 } else if (StripesConstants.SPRING_BEAN_ANNOTATION.equals(annotation.getQualifiedName())) {
                     PsiElement m = annotation.getParent().getParent();
-                    if (m instanceof PsiMethod && StripesUtil.isActionBeanPropertySetter((PsiMethod)m, true)) {
+                    if (m instanceof PsiMethod && !StripesUtil.isActionBeanPropertySetter((PsiMethod)m, true)) {
                         holder.registerProblem(annotation, "Applied to public setter", new RemoveAnnotationQuickFix(annotation, (PsiModifierListOwner)m));
                     }
                 }
