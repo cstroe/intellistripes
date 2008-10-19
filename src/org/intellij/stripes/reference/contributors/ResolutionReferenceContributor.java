5d54551826da4a2da02f6c33dc1afa354c93e25d
Mario.Arias.C <Mario.Arias.C@57c81fc5-d53a-0410-bfd8-01a933467fb7>
2008-10-19 19:31:41 +0000

    param tag support inside form; Apache License for new files;

--- src/org/intellij/stripes/reference/contributors/ResolutionReferenceContributor.java
+++ src/org/intellij/stripes/reference/contributors/ResolutionReferenceContributor.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2000-2007 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.intellij.stripes.reference.contributors;
 
 import com.intellij.psi.PsiClass;
@@ -23,25 +40,24 @@ public class ResolutionReferenceContributor {
 
         registry.registerReferenceProvider(
                 new OrFilter(
-                    new StringArrayAnnotationParameterFilter(StripesConstants.VALIDATION_METHOD_ANNOTATION, StripesConstants.ON_ATTR),
-                    new StringArrayAnnotationParameterFilter(StripesConstants.VALIDATE_ANNOTATION, StripesConstants.ON_ATTR),
-                    new StringArrayAnnotationParameterFilter(StripesConstants.AFTER_ANNOTATION, StripesConstants.ON_ATTR),
-                    new StringArrayAnnotationParameterFilter(StripesConstants.BEFORE_ANNOTATION, StripesConstants.ON_ATTR)
+                        new StringArrayAnnotationParameterFilter(StripesConstants.VALIDATION_METHOD_ANNOTATION, StripesConstants.ON_ATTR),
+                        new StringArrayAnnotationParameterFilter(StripesConstants.VALIDATE_ANNOTATION, StripesConstants.ON_ATTR),
+                        new StringArrayAnnotationParameterFilter(StripesConstants.AFTER_ANNOTATION, StripesConstants.ON_ATTR),
+                        new StringArrayAnnotationParameterFilter(StripesConstants.BEFORE_ANNOTATION, StripesConstants.ON_ATTR)
                 ), PsiLiteralExpression.class, new PsiReferenceProviderBase() {
 
-                @NotNull
-                public PsiReference[] getReferencesByElement(PsiElement psiElement) {
-                    PsiClass cls = PsiTreeUtil.getParentOfType(psiElement, PsiClass.class);
-                    return null == cls
+            @NotNull
+            public PsiReference[] getReferencesByElement(PsiElement psiElement) {
+                PsiClass cls = PsiTreeUtil.getParentOfType(psiElement, PsiClass.class);
+                return null == cls
                         ? PsiReference.EMPTY_ARRAY
                         : new PsiReference[]{new JavaStringResolutionMethodsReference((PsiLiteralExpression) psiElement, cls)};
-                }
+            }
         });
 
         NewOnwardResolutionMethodsReferenceProvider referenceProvider = new NewOnwardResolutionMethodsReferenceProvider();
         registry.registerReferenceProvider(new ParentElementFilter(new NewForwardResolutionFilter()), PsiLiteralExpression.class, referenceProvider);
         registry.registerReferenceProvider(new ParentElementFilter(new NewRedirectResolutionFilter()), PsiLiteralExpression.class, referenceProvider);
-
 
 
     }
