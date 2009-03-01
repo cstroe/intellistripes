7cc612bc123692852180c9251e12ea4341cdc2e1
eshepelyuk <eshepelyuk@57c81fc5-d53a-0410-bfd8-01a933467fb7>
2009-03-01 17:28:45 +0000

    auto-complete of label and sort attributes in option-enumeration tags

--- branches/DIANA/src/org/intellij/stripes/reference/GetterReference.java
+++ branches/DIANA/src/org/intellij/stripes/reference/GetterReference.java
@@ -17,10 +17,12 @@
 
 package org.intellij.stripes.reference;
 
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReferenceBase;
 import com.intellij.psi.util.PropertyUtil;
+import org.apache.commons.lang.ArrayUtils;
 import org.intellij.stripes.util.StripesConstants;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,19 +30,34 @@ import java.util.Arrays;
 
 public class GetterReference extends PsiReferenceBase<PsiElement> {
 
-    protected PsiClass actionBeanPsiClass;
+	protected PsiClass actionBeanPsiClass;
 
-    public GetterReference(PsiElement psiElement, PsiClass actionBeanPsiClass) {
-        super(psiElement);
-        this.actionBeanPsiClass = actionBeanPsiClass;
-    }
+	private String[] variants;
 
-    @Nullable
-    public PsiElement resolve() {
-        return PropertyUtil.findPropertyGetter(actionBeanPsiClass, getValue(), false, true);
-    }
+	public GetterReference(PsiElement psiElement, TextRange range, PsiClass actionBeanPsiClass, String... variants) {
+		super(psiElement, range);
+		this.actionBeanPsiClass = actionBeanPsiClass;
+		this.variants = variants;
+	}
 
-    public Object[] getVariants() {
-        return StripesReferenceUtil.getVariants(Arrays.asList(PropertyUtil.getReadableProperties(actionBeanPsiClass, true)), StripesConstants.FIELD_ICON);
-    }
+	public GetterReference(PsiElement psiElement, PsiClass actionBeanPsiClass) {
+		super(psiElement);
+		this.actionBeanPsiClass = actionBeanPsiClass;
+	}
+
+	@Nullable
+	public PsiElement resolve() {
+		String value = getValue();
+		if (!ArrayUtils.contains(variants, value)) {
+			return PropertyUtil.findPropertyGetter(actionBeanPsiClass, value, false, true);
+		}
+		return myElement;
+	}
+
+	public Object[] getVariants() {
+		String[] retval = PropertyUtil.getReadableProperties(actionBeanPsiClass, true);
+		if (variants != null) retval = (String[]) ArrayUtils.addAll(retval, variants);
+
+		return StripesReferenceUtil.getVariants(Arrays.asList(retval), StripesConstants.FIELD_ICON);
+	}
 }
--- branches/DIANA/src/org/intellij/stripes/reference/contributors/GetterReferenceContributor.java
+++ branches/DIANA/src/org/intellij/stripes/reference/contributors/GetterReferenceContributor.java
@@ -16,10 +16,12 @@
  */
 package org.intellij.stripes.reference.contributors;
 
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.reference.PsiReferenceProviderBase;
 import com.intellij.psi.jsp.el.ELExpressionHolder;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.ReferenceSetBase;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.ProcessingContext;
 import org.intellij.stripes.components.project.StripesReferencesComponent;
@@ -48,10 +50,34 @@ public class GetterReferenceContributor extends PsiReferenceContributor {
 	};
 
 	public void registerReferenceProviders(PsiReferenceRegistrar registrar) {
-		StripesReferencesComponent.registerXmlAttributeReferenceProviderAttr(registrar, REFERENCE_PROVIDER,
-			StripesConstants.OPTIONS_ENUMERATION_TAG, StripesConstants.SORT_ATTR, StripesConstants.LABEL_ATTR);
-
 		StripesReferencesComponent.registerXmlAttributeReferenceProvider(registrar, REFERENCE_PROVIDER,
-			StripesConstants.LABEL_ATTR, StripesConstants.OPTIONS_COLLECTION_TAG);
+			StripesConstants.LABEL_ATTR, StripesConstants.OPTIONS_ENUMERATION_TAG);
+
+		StripesReferencesComponent.registerXmlAttributeReferenceProvider(registrar, new PsiReferenceProviderBase() {
+			@NotNull
+			@Override
+			public PsiReference[] getReferencesByElement(@NotNull final PsiElement element, @NotNull ProcessingContext context) {
+				if (PsiTreeUtil.getChildOfType(element, ELExpressionHolder.class) != null) {
+					return PsiReference.EMPTY_ARRAY;
+				}
+
+				XmlTag tag = (XmlTag) element.getParent().getParent();
+				final PsiClass actionBeanPsiClass = StripesUtil.findPsiClassByName(tag.getAttributeValue(StripesConstants.ENUM_ATTR), element.getProject());
+
+				if (null != actionBeanPsiClass) {
+					return new ReferenceSetBase<PsiReference>(ElementManipulators.getValueText(element), element, 1, ',') {
+						@NotNull
+						protected PsiReference createReference(TextRange range, int index) {
+//							getReferences().add(new StaticReference(element, range, "sort", "label"));
+							return new GetterReference(element, range, actionBeanPsiClass, "sort", "label");
+						}
+					}.getPsiReferences();
+				}
+				return PsiReference.EMPTY_ARRAY;
+			}
+		}, StripesConstants.SORT_ATTR, StripesConstants.OPTIONS_ENUMERATION_TAG);
+
+//		StripesReferencesComponent.registerXmlAttributeReferenceProvider(registrar, REFERENCE_PROVIDER,
+//			StripesConstants.LABEL_ATTR, StripesConstants.OPTIONS_COLLECTION_TAG);
 	}
 }
